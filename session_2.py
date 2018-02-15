9a1c433af6b999e03d49bc03f0695dd42b6f51a8
aarthi3193 <36494557+aarthi3193@users.noreply.github.com>
2018-02-14 21:27:40 -0500

    Add files via upload

---
+++ session_2.py
@@ -0,0 +1,161 @@
+
+# coding: utf-8
+
+# d = 10 == 10
+# print(d)
+# 
+
+# In[2]:
+
+
+print(type(d))
+
+
+# ### Data Types
+# ##### int
+
+# In[2]:
+
+
+a = 10
+
+
+# ## LIST
+
+# In[4]:
+
+
+l = [1,2,3,4,5]
+print (l)
+
+
+# In[5]:
+
+
+print(type(l))
+
+
+# In[15]:
+
+
+my_list_str = [ "Hi","Hello",1,2,3,4.5,True]
+
+
+# In[7]:
+
+
+my_list_str[1][4]
+
+
+# In[8]:
+
+
+l[1]
+
+
+# In[16]:
+
+
+my_list_str[0:6]
+
+
+# In[20]:
+
+
+my_list_str[-2:]
+
+
+# In[21]:
+
+
+print(my_list_str)
+
+
+# In[32]:
+
+
+my_list_str[-3:-6]
+
+
+# In[23]:
+
+
+my_list_str.append(100)
+
+
+# In[24]:
+
+
+print (my_list_str)
+
+
+# In[41]:
+
+
+my_list_str[2:-2]
+
+
+# In[45]:
+
+
+#examples for exh function of list
+my_list_str.extend([7, 8, 9])
+
+
+# In[46]:
+
+
+print(my_list_str)
+
+
+# In[51]:
+
+
+#examples for exh function of list
+my_list_str.insert(4,5)
+
+
+# In[52]:
+
+
+print(my_list_str)
+
+
+# In[53]:
+
+
+my_list_str.pop(1)
+
+
+# In[54]:
+
+
+my_list_str.pop()
+
+
+# In[61]:
+
+
+my_list_str.append(5)
+my_list_str.remove('Hi')
+my_list_str.sort()
+my_list_str.index(5)
+my_list_str.reverse()
+print(my_list_str)
+
+
+# ## List Methods
+# 
+
+# ##### append
+# ####  extend
+# ####  pop
+# #### remove
+# #### insert
+# #### clear
+# #### index
+# #### count
+# #### start
+# #### reverse
+# #### copy
+# 
