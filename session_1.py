ce982143d2273f527a2c2a568223268fe10f07aa
aarthi3193 <36494557+aarthi3193@users.noreply.github.com>
2018-02-14 21:27:15 -0500

    Add files via upload

---
+++ session_1.py
@@ -0,0 +1,67 @@
+
+# coding: utf-8
+
+# In[1]:
+
+
+print("Hello World!")
+
+
+# In[1]:
+
+
+a = 10
+
+
+# In[3]:
+
+
+print (a)
+
+
+# In[4]:
+
+
+type (a)
+
+
+# In[5]:
+
+
+a = 20.5
+
+
+# In[6]:
+
+
+print (a)
+
+
+# In[7]:
+
+
+type(a)
+
+
+# In[8]:
+
+
+b = "Hello"
+
+
+# In[9]:
+
+
+type(b)
+
+
+# In[13]:
+
+
+c = 10.5
+d = True
+print(c)
+print(d)
+print(type(c))
+print(type(d))
+
