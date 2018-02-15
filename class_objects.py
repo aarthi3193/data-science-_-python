2b8f5e0e4eddee938fc0382c4ab650850d22e219
aarthi3193 <36494557+aarthi3193@users.noreply.github.com>
2018-02-14 21:32:18 -0500

    Add files via upload

---
+++ class_objects.py
@@ -0,0 +1,34 @@
+
+# coding: utf-8
+
+# In[8]:
+
+
+class my_class:
+    a = 10
+    def add (self,a,b):
+        return(a+b)
+    def sub (self,x,y):
+        return(x-y)
+    def mult (self,c,d):
+        return(c*d)
+    def div (self,p,q):
+        return(p%q)
+
+
+
+
+
+# In[9]:
+
+
+obj1 = my_class()
+obj1.add(10,20)
+
+
+# In[10]:
+
+
+obj1 = my_class()
+obj1.mult(10,20)
+
