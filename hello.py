05c304f7aa2e03c0be03c849996f363517f1cca8
aarthi3193 <36494557+aarthi3193@users.noreply.github.com>
2018-02-14 23:19:20 -0500

    Add files via upload

---
+++ hello.py
@@ -0,0 +1,17 @@
+print("hello")
+l = [1,2,3,4]
+print(l)
+
+#convert all notebooks to .py files
+#basic linux commands
+#basics of vi commands
+#inheritance
+#basics of oops
+#write a python code for inheritance and why it is used?
+
+#pycharm
+#spyder
+#sublime
+
+#IDE (Integrated Development Environment)
+
