36425fd2b9d2ded7f3460aad73bf8cbf4358ebc3
aarthi3193 <36494557+aarthi3193@users.noreply.github.com>
2018-02-19 22:21:35 -0500

    Add files via upload

---
+++ arithmetic_operations.py
@@ -0,0 +1,13 @@
+def add(a,b):
+    print ("called adding")
+    return a+b
+
+def sub(a,b):
+    print ("called sub")
+    return a-b
+def mult(a,b):
+    print("mult called")
+    return a*b
+def div(a,b):
+    print("division called")
+    return a%b
